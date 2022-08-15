c3609e26e0a73f0ae70e99b7a9b4e5b0ffa830b3
Colby M. White <colbywhite@users.noreply.github.com>
2022-08-15 11:43:17 -0500

    feat: add tailwind
    yarn workspace web add -D sass sass-loader
    yarn rw setup ui tailwindcss

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,33 @@
 import { MetaTags } from "@redwoodjs/web";
 
+function Header() {
+  return (
+    <header className="flex flex-row items-center justify-between">
+      <MetaTags title="Redwood template" description="Home page" />
+      <div className="flex flex-row items-center gap-4">
+        <img src="/redwood.svg" className="motion-safe:animate-spin-slow h-10 pointer-events-none" alt="logo" />
+        <h6 className="text-xl font-medium">Redwood template</h6>
+      </div>
+    </header>
+  );
+}
+
 const HomePage = () => {
   return (
-    <>
-      <MetaTags title="Home" description="Home page" />
-      <main>
+    <div className="max-w-2xl flex flex-col gap-8 my-4 mx-auto">
+      <Header />
+      <main className="flex flex-col gap-4">
         <p>
           This is a starting template for a <code>redwood</code> project with what I consider a sane set of
           starting settings.
         </p>
-        <ul>
+        <ul className="list-disc mx-auto">
           <li>The basic "Hello, World" page you're looking at now</li>
+          <li><code>tailwindcss</code></li>
+          <li>CSS reset via <code>tailwindcss</code></li>
         </ul>
       </main>
-    </>
+    </div>
   );
 };
 
